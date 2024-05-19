fce42d54f64c5bbb580e53c0628d0a6ebda0a98d
Dilmin Tharusha <>
2024-05-19 23:59:19 +0530

    Version 1

---
+++ src/Model/MySQL.java
@@ -0,0 +1,38 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySQL {
+
+    private static Connection connection;
+    private static final String userName = "root";
+    private static final String password = "your password"; //enter your password
+    private static final String database = "foodcity_db";
+
+    static {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, userName, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static ResultSet exequte(String query) throws Exception {
+        Statement statement = connection.createStatement();
+
+        if (query.startsWith("SELECT")) {
+            return statement.executeQuery(query);
+        } else {
+            statement.executeUpdate(query);
+            return null;
+        }
+    }
+
+}
